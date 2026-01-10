166948d2bf38fc8c3577a51a8c47a747a7c5c972
Shmuel atar <shmuelatar@gmail.com>
2026-01-10 19:11:33 +0200

    Adds UserSettingForm component and AppSideProvider context for user settings management

---
+++ client/src/contexts/AppSideProvider.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { createContext, useContext, useState } from "react";
+import { useTranslation } from "react-i18next";
+
+const AppSideContext = createContext();
+
+function AppSideProvider({ children }) {
+    const { i18n } = useTranslation()
+    const [isRight, setIsRight] = useState(true);
+    const [side, setSide] = useState("right");
+    const [opSide, setOpSide] = useState("left");
+
+    const toggleSide = (lang) => {
+        i18n.changeLanguage(lang)
+        setIsRight(!isRight);
+        setSide(isRight ? "left" : "right");
+        setOpSide(isRight ? "right" : "left");
+
+    }
+
+    // Set initial side based on browser language
+    useEffect(() => {
+        const browserLang =
+            navigator.languages?.[0] || navigator.language || "en";
+
+        const lang = browserLang.split("-")[0];
+
+        if (lang === "he") {
+            toggleSide(lang)
+            document.documentElement.lang = lang;
+        }
+    }, [])
+
+    return <AppSideContext.Provider value={{ side, isRight, opSide, toggleSide, language: i18n.language }}>{children}</AppSideContext.Provider>
+}
+
+function useAppSide() {
+    const context = useContext(AppSideContext);
+    if (context === undefined) throw new Error('AppSideContext was used outside of the provider')
+    return context
+}
+
+export { AppSideProvider, useAppSide }
